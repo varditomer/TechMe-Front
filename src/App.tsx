857608f4d89ca6281f3550cfa6bf8e3d44e7daae
varditomer <tomer.vardi@gmail.com>
2025-05-09 11:33:44 +0300

    Setup base React TS project with MUI, Router, Axios and clean structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<div>Welcome to TechMe 👋</div>} />
+        </Routes>
+      </div>
     </>
   );
 }
